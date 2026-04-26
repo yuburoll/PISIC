e448339b82d3e17d87b4da4db105110c23a7fee6
yuburoll <yuburoll@gmail.com>
2026-04-26 21:09:24 +0900

    edit keymap with keypads

--- firmware/nologo rp2040 qmk/pisic/keymaps/tweaked/keymap.c
+++ firmware/nologo rp2040 qmk/pisic/keymaps/tweaked/keymap.c
@@ -8,22 +8,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPC, 
                 KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                           KC_H,           KC_J,           KC_K,           KC_L,           KC_ENT,
         KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_B,           KC_N,           KC_M,           KC_COMM,        KC_DOT,         RSFT_T(KC_SLSH), 
-        KC_LCTL,        KC_LGUI,                        KC_LALT,        KC_TAB,         KC_SPC,         KC_SPC,         KC_RALT,        KC_RCTL,                        LT(2,KC_APP),   LT(1,KC_DEL)),
+        KC_LCTL,        KC_LGUI,                        KC_LALT,        KC_TAB,         KC_SPC,         LT(1,KC_SPC),   KC_RALT,        KC_RCTL,                        LT(3,KC_APP),   LT(2,KC_DEL)),
     [1] = LAYOUT_split_656_5(
         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,        KC_EQL,
                 KC_TAB,         KC_ESC,         KC_GRV,         KC_NO,          KC_NO,                          KC_RBRC,        KC_LBRC,        KC_UP,          KC_QUOT,        KC_SCLN, 
         KC_TRNS,        KC_CAPS,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_BSLS,        KC_LEFT,        KC_DOWN,        KC_RGHT,        KC_RSFT, 
-        KC_TRNS,        KC_TRNS,                        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_RALT,                        MO(3),          KC_TRNS),
+        KC_TRNS,        KC_TRNS,                        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_RALT,                        KC_BSPC,        KC_DEL),
     [2] = LAYOUT_split_656_5(
         KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,  
                 KC_HOME,        KC_END,         KC_PGUP,        KC_PGDN,        KC_INS,                         KC_NO,          MS_WHLU,        MS_UP,          MS_WHLL,        MS_WHLR,
         KC_TRNS,        KC_NO,          KC_NO,          KC_PSCR,        KC_SCRL,        KC_BRK,         KC_NO,          MS_WHLD,        MS_LEFT,        MS_DOWN,        MS_RGHT,        KC_RSFT, 
         KC_TRNS,        KC_TRNS,                        KC_TRNS,        KC_TRNS,        KC_TRNS,        MS_BTN1,        MS_BTN3,        MS_BTN2,                        KC_TRNS,        KC_NO),
     [3] = LAYOUT_split_656_5(
-        UG_TOGG,        UG_NEXT,        UG_SPDU,        UG_HUEU,        UG_SATU,        UG_VALU,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-                BT_PRF1,        BT_PRF2,        BT_PRF3,        OU_AUTO,        BT_UNPR,                        KC_NO,          QK_BOOT,        QK_RBT,         KC_NO,          KC_NO,
-        KC_TRNS,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_NO,                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                          KC_TRNS,        KC_NO)
+        UG_TOGG,        UG_NEXT,        UG_SPDU,        UG_HUEU,        UG_SATU,        UG_VALU,        KC_P7,          KC_P8,          KC_P9,          KC_PSLS,        KC_PAST,        KC_BSPC,
+                BT_PRF1,        BT_PRF2,        BT_PRF3,        OU_AUTO,        BT_UNPR,                        KC_P4,          KC_P5,          KC_P6,          KC_PMNS,        KC_PENT,
+        KC_TRNS,        KC_NO,          KC_NO,          KC_NO,          QK_BOOT,        QK_RBT,         KC_NO,          KC_P1,          KC_P2,          KC_P3,          KC_PPLS,        KC_RSFT,
+        KC_NO,          KC_NO,                          KC_NO,          KC_NO,          KC_NO,          KC_P0,          KC_PCMM,        KC_PDOT,                        KC_TRNS,        KC_NO)
 };
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
